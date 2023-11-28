cc74ea3a8ae7397225ad57b18fb4cbff2ef47def
amberlynn <amberlynn364@gmail.com>
2023-11-28 22:13:01 +0300

    feat: add links at Main page

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,3 +1,19 @@
+import { Link } from 'react-router-dom';
+import styles from './Main.module.css';
+import RouterPath from '../../router/routerTypes';
+
 export default function Main() {
-  return <div>Main</div>;
+  return (
+    <main className={styles.main}>
+      <h1>Main Page</h1>
+      <div className={styles.linksWrapper}>
+        <Link to={RouterPath.ReactHookForms} className={styles.link}>
+          React Hook Forms
+        </Link>
+        <Link to={RouterPath.UncontrolledForms} className={styles.link}>
+          Uncontrolled Forms
+        </Link>
+      </div>
+    </main>
+  );
 }
